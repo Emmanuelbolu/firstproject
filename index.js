b948a4b654ade4f5baa6bb7dbb4538df51f12792
Emmanuelbolu <kuyorobolu29@gmail.com>
2022-02-02 09:36:17 +0100

    done with the mobile view

---
+++ index.js
@@ -0,0 +1,8 @@
+function myfunction(params) {
+    var x = document.getElementById("box3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }   
+}
